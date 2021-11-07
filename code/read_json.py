f273c666b7b4e3afc17c0065cb3ece0f08402479
Shubo Lin <slin268@wisc.edu>
2021-11-07 13:25:21 -0600

    update

--- code/read_json.py
+++ code/read_json.py
@@ -1,2 +1,9 @@
 import json
-import
+import numpy as np
+import pandas as pd
+
+with open("../yelp_dataset/business.json", encoding = "utf-8") as f:
+    business = json.load(f)
+
+
+business = pd.json_normalize(business)
