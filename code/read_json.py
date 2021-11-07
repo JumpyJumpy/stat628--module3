0cbec922ca9463e303cd75a0f4d60339b92badb9
Shubo Lin <slin268@wisc.edu>
2021-11-07 14:11:59 -0600

    update

--- code/read_json.py
+++ code/read_json.py
@@ -2,8 +2,7 @@ import json
 import numpy as np
 import pandas as pd
 
-with open("yelp_dataset/business.json", encoding = "utf-8") as f:
-    business = json.load(f)
-
-
 business = pd.read_json("yelp_dataset/business.json", lines = True)
+
+with open("yelp_dataset/review.json", encoding = "utf-8") as f:
+    review = json.load(f)  # does not work, MemoryError
