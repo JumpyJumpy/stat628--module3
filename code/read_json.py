72606db5353ca3e98da046044fb366403534bdb9
Shubo Lin <slin268@wisc.edu>
2021-11-08 10:54:31 -0600

    update

--- code/read_json.py
+++ code/read_json.py
@@ -3,6 +3,17 @@ import numpy as np
 import pandas as pd
 
 business = pd.read_json("yelp_dataset/business.json", lines = True)
+business = business[business["categories"].str.contains("Food", na = False)]
+ids = business["business_id"].tolist()
+business.describe()
+business.to_csv("business")
 
+review = pd.DataFrame()
 with open("yelp_dataset/review.json", encoding = "utf-8") as f:
-    review = json.load(f)  # does not work, MemoryError
+    for line in f:
+        tmp = json.loads(line)
+        if tmp["business_id"] in ids:
+            review = review.append(tmp, ignore_index = True)
+    #  review = json.load(f)  # does not work, MemoryError
+
+review.to_csv("review.csv")
