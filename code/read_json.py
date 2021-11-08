866d7ede5a1860155bf5f33d5d77fb100e5f1275
Shubo Lin <slin268@wisc.edu>
2021-11-08 13:24:35 -0600

    update

--- code/read_json.py
+++ code/read_json.py
@@ -3,9 +3,8 @@ import numpy as np
 import pandas as pd
 
 business = pd.read_json("yelp_dataset/business.json", lines = True)
-business = business[business["categories"].str.contains("Food", na = False)]
+business = business[business["categories"].str.contains("Cafe", na = False)]
 ids = business["business_id"].tolist()
-business.describe()
 business.to_csv("business.csv")
 
 review = pd.DataFrame()
