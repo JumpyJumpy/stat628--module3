bbd5d16ff3b2ba38b07553b03af22c65bd558f54
Shubo Lin <slin268@wisc.edu>
2021-11-11 01:05:36 -0600

    update

--- code/anova.R
+++ code/anova.R
@@ -3,3 +3,7 @@ library("car")
 library("MASS")
 
 business <- read_csv("business_flattened.csv")
+review <- read_csv("filtered_reviews.csv")
+review$business_id <- factor(review$business_id)
+
+star_avg <- tapply()
--- code/attribute_frequency.py
+++ code/attribute_frequency.py
@@ -9,14 +9,14 @@ attributes = [keys for i in range(len(business["attributes"])) for keys in busin
 attributes = pd.Series(attributes)
 print(attributes.value_counts())
 
-counts = attributes.value_counts().to_dict()
-keys = list(counts.keys())
+attributes_counts = attributes.value_counts().to_dict()
+attributes_keys = list(attributes_counts.keys())
 
-business_flattened = pd.concat([business.iloc[:, 0], pd.DataFrame(np.nan, columns = keys, index = business.index)],
+business_flattened = pd.concat([business.iloc[:, 0], pd.DataFrame(np.nan, columns = attributes_keys, index = business.index)],
                                axis = 1)
 
-for idx in business_flattened.index:
+for idx in business_flattened.index.to_list():
     for keys in business.loc[idx, "attributes"]:
-        business_flattened.loc[idx, keys] = bool(business.loc[idx, "attributes"][keys])
+        business_flattened.loc[idx, keys] = int(bool(business.loc[idx, "attributes"][keys]))
 
 business_flattened.to_csv("business_flattened.csv")
