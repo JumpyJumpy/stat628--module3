61728d0c82312dde8fb6d52d4380ba3f1f7b0e92
Shubo Lin <slin268@wisc.edu>
2021-11-11 01:11:42 -0600

    update

--- code/anova.R
+++ code/anova.R
@@ -6,4 +6,7 @@ business <- read_csv("business_flattened.csv")
 review <- read_csv("filtered_reviews.csv")
 review$business_id <- factor(review$business_id)
 
-star_avg <- tapply()
+star_avg <- tapply(review$stars, review$business_id, mean)
+star_avg <- data.frame(stars = star_avg, business_id = names(star_avg))
+
+dataset <- merge(business, star_avg, by = "business_id")
