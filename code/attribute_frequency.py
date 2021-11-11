d2ff320c12297b7971d313cf26ff3ec314a11422
Shubo Lin <slin268@wisc.edu>
2021-11-10 21:21:35 -0600

    update

--- code/attribute_frequency.py
+++ code/attribute_frequency.py
@@ -5,11 +5,7 @@ import ast
 
 business = pd.read_csv("business.csv")
 nested_attributes = [ast.literal_eval(i) for i in business["attributes"].to_list() if i is not np.nan]
-
-attributes = []
-for i in range(len(nested_attributes)):
-    for keys in nested_attributes[i]:
-        attributes.append(keys)
+attributes = [keys for j in range(len(nested_attributes)) for keys in nested_attributes[j]]
 
 attributes = pd.Series(attributes)
 print(attributes.value_counts())
