98d6543e3be343b76b55fcbd47f43228779f5d73
Shubo Lin <slin268@wisc.edu>
2021-11-11 00:39:43 -0600

    update

--- code/attribute_frequency.py
+++ code/attribute_frequency.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
 
-business = pd.read_csv("business.csv")
-nested_attributes = [eval(i) for i in business["attributes"].to_list() if i is not np.nan]
-attributes = [keys for j in range(len(nested_attributes)) for keys in nested_attributes[j]]
+business = pd.read_csv("business.csv", index_col = 0)
+business = business.loc[~business["attributes"].isna(), :]
+business["attributes"] = [eval(attr) for attr in business["attributes"]]
+attributes = [keys for i in range(len(business["attributes"])) for keys in business["attributes"].iloc[i]]
 
 attributes = pd.Series(attributes)
 print(attributes.value_counts())
@@ -11,3 +12,11 @@ print(attributes.value_counts())
 counts = attributes.value_counts().to_dict()
 keys = list(counts.keys())
 
+business_flattened = pd.concat([business.iloc[:, 0], pd.DataFrame(np.nan, columns = keys, index = business.index)],
+                               axis = 1)
+
+for idx in business_flattened.index:
+    for keys in business.loc[idx, "attributes"]:
+        business_flattened.loc[idx, keys] = bool(business.loc[idx, "attributes"][keys])
+
+business_flattened.to_csv("business_flattened.csv")
