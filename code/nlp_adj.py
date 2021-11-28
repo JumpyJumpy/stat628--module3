2412fab9523651df815d2742b49736489290ea3d
Shubo Lin <slin268@wisc.edu>
2021-11-27 19:12:35 -0600

    update

--- code/nlp_adj.py
+++ code/nlp_adj.py
@@ -1,7 +1,19 @@
 import numpy as np
 import pandas as pd
 import re
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+reviews = pd.read_csv("./data/filtered_reviews.csv")
+
+try:
+    stop_words = stopwords.words("english")
+except LookupError:
+    nltk.download(stopwords)
 
 
+reviews["text"] = reviews["text"].str.lower()
+reviews["text"] = reviews["text"].str.replace(pat = "n\'t ", repl = "not")
+reviews["text"] = reviews["text"].str.replace(pat = "not ", repl = "not_")
 
-pd.read_csv("./data/")
