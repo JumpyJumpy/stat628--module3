9c201795e8521544878fc383c3690c454f15ff0a
Shubo Lin <slin268@wisc.edu>
2021-12-06 12:12:11 -0600

    update

--- code/word_t_test.R
+++ code/word_t_test.R
@@ -2,48 +2,54 @@ library("tidyverse")
 library("car")
 
 reviews <- read_csv("./data/filtered_reviews.csv")
-words <- read_csv("./data/tf_idf_words.csv", na = c("", "NA", "NaN"))
+words <- read_csv("./data/tf_idf_words.csv", na = c("", "NA", "NaN"))[-1]
+label <- read_csv("./data/word_list.csv",)[, -1]
 
 business_id <- unique(reviews$business_id)
-suggestions <- data.frame(business_id = business_id, word1 = NA, word2 = NA, word1_label = NA, word2_label = NA, suggestion = NA)
+suggestions <- data.frame(business_id = business_id, word1 = NA, word2 = NA, word1_label = NA, word2_label = NA, suggestion1 = NA, suggestion2 = NA)
 
 
 for (id in business_id) {
     reviews_selected <- reviews[reviews$business_id == id,]
     top1 <- words[words$business_id == id, "1"]
     top2 <- words[words$business_id == id, "2"]
-    
-    if(!is.na(top1)) {
+
+    if (!is.na(top1)) {
         top1 <- as.character(top1)
-        lable1 <- 
-            top1_stars <- reviews_selected$stars[grep(top1, reviews_selected$text)]
-        top1_test <- t.test(x = top1_stars, y = reviews_selected$stars, alternative = "less")  
-        
+        label1 <- label$Kind[label$Var1 == top1]
+        top1_stars <- reviews_selected$stars[grep(top1, reviews_selected$text)]
+        top1_test <- t.test(x = top1_stars, y = reviews_selected$stars, alternative = "less")
+
         if (top1_test$p.value < 0.05) {
             sugg1 <- paste0("You should improve the quality of ", top1, "There is significant ")
         } else {
-            sugg1 <- 
-                paste0("You are doing well in ", top1, ".\nBecause comments related to ", top1, "is not statistically lower than your average rating. se( ", top1_test$stderr, ")")
+            sugg1 <-
+                    paste0("You are doing well in ", top1, ".\nBecause comments related to ", top1, "is not statistically lower than your average rating. se( ", top1_test$stderr, ")")
         }
     } else {
         sugg1 <- NA
         label1 <- NA
-        suggestions[suggestions$business_id == id, 2:6] <- c()
     }
-    
-    
-    if (!is.na(top2))
+
+    if (!is.na(top2)) {
+        top2 <- as.character(top2)
+        label2 <- label$Kind[label$Var1 == top2]
         top2_stars <- reviews_selected$stars[grep(top2, reviews_selected$text)]
-    top2_test <- t.test(x = top2_stars, y = reviews_selected$stars, alternative = "less")
-    
-    if (top1_test$p.value < 0.05) {
-        sugg1 <- paste0("You should improve the quality of ", top1)
-        reason1 <- paste0("There is significant ")
+        top2_test <- t.test(x = top2_stars, y = reviews_selected$stars, alternative = "less")
+
+        if (top1_test$p.value < 0.05) {
+            sugg2 <- paste0("You should improve the quality of ", top1, "There is significant ")
+        } else {
+            sugg2 <-
+                    paste0("You are doing well in ", top1, ".\nBecause comments related to ", top1, "is not statistically lower than your average rating. se( ", top1_test$stderr, ")")
+        }
     } else {
-        sugg1 <- paste0("You are doing well in ", top1)
-        reason1 <- paste0("Because comments related to ", top1, "is not statistically lower than your average rating. se( ", top1_test$stderr, ")")
+        sugg2 <- NA
+        label2 <- NA
+
     }
-    
->>>>>>> 32bd3bad1839dfecc16ae80143c626c3c816464d
+
+    suggestions[suggestions$business_id == id, 2:7] <- c(top1, top2, label1, label2, sugg1, sugg2)
+    break
 }
 
