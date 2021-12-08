a027d075d4f2041ec656497ad4006ad492570544
nvarshney20 <nvarshney@ucdavis.edu>
2021-12-07 20:31:42 -0600

    Updated Shiny App

--- code/Recommendations/app.R
+++ code/Recommendations/app.R
@@ -1,7 +1,14 @@
 library(shiny)
 library(dplyr)
+library(ggplot2)
+library(stringr)
 
-business <- read.csv('../../data/business.csv')
+business <- read.csv('../../data/business.csv') %>%
+    mutate(across(where(is.factor), as.character))
+suggestions <- read.csv('../../data/suggestions.csv') %>%
+    mutate(across(c(suggestion1, suggestion2, suggestions3),
+                  ~str_replace_all(., '\n', ' '))) %>%
+    mutate(across(where(is.factor), as.character))
 
 ui <- fluidPage(
     titlePanel('Recommendations Based on Yelp Reviews'),
@@ -10,39 +17,102 @@ ui <- fluidPage(
             selectInput('State', 'State:', choices = unique(business$state),
                         selected = 'OR'),
             selectInput('City', 'City:', choices = '', selected = 'Portland'),
-            selectInput('Business', 'Business:',
-                        choices = '',
-                        selected = 'Flying Elephants at PDX (7000 NE Airport Way)'),
+            selectInput('Name', 'Name:', choices = '', selected = 'Flying Elephants at PDX'),
+            selectInput('Address', 'Address:', choices = '',
+                        selected = '7000 NE Airport Way')
         ),
         mainPanel(
-            tableOutput('Reccos')
+            plotOutput('RankBar', width = '75%', height = '300px'),
+            htmlOutput('Rank'),
+            h3('Suggestions'),
+            htmlOutput('Words'),
+            br(),
+            htmlOutput('Suggestions')
         )
     )
 )
 
 server <- function(input, output, session) {
     observe({
-        updateSelectInput(session, 'City', choices =
-                              (business %>% filter(state == input$State))$city %>%
-                              unique)
-    })
-    observe({
-        updateSelectInput(session, 'Business', choices = paste(
-            (
-                business %>% filter(state == input$State) %>%
-                    filter(city == input$City)
-            )$name,
-            ' (', (
-                business %>% filter(state == input$State) %>%
-                    filter(city == input$City)
-            )$address, ')', sep = '')
+        updateSelectInput(session, 'City', choices = (
+            business %>% filter(state == input$State))$city %>% unique
         )
     })
-    output$Reccos <- renderTable({
-        reccs <- data.frame(cbind('This is what the business does well',
-                                  'This is what the business needs to improve.'))
-        colnames(reccs) <- c('Strengths', 'Weaknesses')
-        reccs
+    observe({
+        updateSelectInput(session, 'Name', choices = (
+            business %>% filter(state == input$State & city == input$City))$name 
+        )
+    })
+    observe({
+        updateSelectInput(session, 'Address', choices = (
+            business %>% filter(
+                state == input$State & city == input$City & name == input$Name
+            ))$address
+        )
+    })
+    output$RankBar <- renderPlot({
+        if(input$State == '' | input$City == '' | input$Name == '' | input$Address == '') {
+            return(NULL)
+        }
+        businesses_city <- (business %>% filter(
+            state == input$State & city == input$City))$business_id
+        ratings_city <- suggestions %>% filter(business_id %in% businesses_city) %>%
+            group_by(business_id) %>% summarize_at(vars(average_rating), mean) %>%
+            arrange(desc(average_rating))
+        bid <- (business %>% filter(
+            state == input$State & city == input$City & name == input$Name &
+                address == input$Address))$business_id
+        ratings_city %>% ggplot(aes(x = average_rating)) + geom_histogram(binwidth = 0.2) +
+            xlab('Average Rating from Reviews (Stars)') + ylab('Number of Businesses') +
+            ggtitle(paste('Average Rating for Businesses in', input$City, input$State)) +
+            geom_vline(xintercept =
+                (ratings_city %>% filter(business_id == bid))$average_rating
+            )
+    })
+    output$Rank <-renderText({
+        if(input$State == '' | input$City == '' | input$Name == '' | input$Address == '') {
+            return(NULL)
+        }
+        businesses_city <- (business %>% filter(
+            state == input$State & city == input$City))$business_id
+        ratings_city <- suggestions %>% filter(business_id %in% businesses_city) %>%
+            group_by(business_id) %>% summarize_at(vars(average_rating), mean) %>%
+            arrange(desc(average_rating))
+        bid <- (business %>% filter(
+            state == input$State & city == input$City & name == input$Name &
+                address == input$Address))$business_id
+        paste('<b>Rank:', which(ratings_city$business_id == bid), 'of',
+              dim(ratings_city)[1], 'businesses</b>')
+    })
+    output$Words <- renderText({
+        if(input$State == '' | input$City == '' | input$Name == '' | input$Address == '') {
+            return(NULL)
+        }
+        id <- (business %>% filter(
+            state == input$State & city == input$City & name == input$Name &
+                address == input$Address))$business_id
+        info <- suggestions %>% filter(business_id == id)
+        words <- c()
+        if(!is.na(info$word1)) words <- c(words, info$word1)
+        if(!is.na(info$word2)) words <- c(words, info$word2)
+        if(!is.na(info$word3)) words <- c(words, info$word3)
+        paste('<b>Most Important Words (Based on Reviews):</b>',
+              paste(words, collapse = ', ')
+        )
+    })
+    output$Suggestions <- renderText({
+        if(input$State == '' | input$City == '' | input$Name == '' | input$Address == '') {
+            return(NULL)
+        }
+        id <- (business %>% filter(
+            state == input$State & city == input$City & name == input$Name &
+                address == input$Address))$business_id
+        info <- suggestions %>% filter(business_id == id)
+        sug <- c()
+        if(!is.na(info$suggestion1)) sug <- c(sug, info$suggestion1)
+        if(!is.na(info$suggestion2)) sug <- c(sug, info$suggestion2)
+        if(!is.na(info$suggestions3)) sug <- c(sug, info$suggestions3)
+        paste(sug, collapse = '<br>')
     })
 }
 
